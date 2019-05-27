601c99b9be2d04792587a68b3426dff7f8762e8d
GODSOWN1 <kennethmaduegbunam@gmail.com>
2019-05-27 15:02:49 +0200

    final.php

---
+++ final.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP QUIZZER</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body background-color="#2980b9">
+<header>
+	<div class="container">
+		<h1>PHP QUIZZER</h1>	
+	</div>
+	</header>
+	<main>
+		<div class="container">
+			<h2>You are done</h2>
+			<p>Congrats! you have completed the test</p>
+			<p>Final score: 5</p>
+			<a href="question.php?n=1" class="start"> TAKE AGAIN</a>
+		</div>
+	</main>
+	<footer>
+		<div class="container">
+			copyright  2019; PHP QUIZZER
+		</div>
+	</footer>
+</body>
+</html>
